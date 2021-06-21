c8b053fddb16fe43b3c6e436f0ce0b81bf5d09b5
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-21 11:32:39 +0530

    Add files via upload

---
+++ Example/Example/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace example
+{
+    class studentInfo
+    {
+
+        private string id;
+        private string name;
+        private int marks;
+
+        public string std_id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string std_name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int std_marks
+        {
+            get { return marks; }
+            set { marks = value; }
+        }
+
+    }
+
+    class stdinfo
+    {
+        static void Main()
+        {
+            studentInfo instance = new studentInfo();
+            instance.std_id = "25";
+            instance.std_name = "Ram";
+            instance.std_marks = 85;
+
+            Console.WriteLine(instance.std_id);
+            Console.WriteLine(instance.std_name);
+            Console.WriteLine(instance.std_marks);
+
+        }
+    }
+
+}
+
