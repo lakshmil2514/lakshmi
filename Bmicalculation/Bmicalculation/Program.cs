eff029537de33db25d8c3d40d2534e7e3988d484
Lakshmi L <51417725+lakshmil2514@users.noreply.github.com>
2021-06-24 16:53:24 +0530

    Add files via upload

---
+++ Bmicalculation/Bmicalculation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bmicalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BMI Calculator");
+            Console.WriteLine();
+
+            Console.Write("Weight in kg: ");
+            int kg;
+            kg = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Height in cm: ");
+            double m;
+            m = Convert.ToDouble(Console.ReadLine());
+
+            //calculation of BMI
+            double BMI;
+            BMI = kg / m * 2;
+
+            if (BMI < 18.5 )
+            { Console.WriteLine("-> Underweight"); }
+            if (BMI >=19 & BMI <= 24)
+            { Console.WriteLine("-> Healthy"); }
+            if (BMI >=25)
+            { Console.WriteLine("-> Overweight"); }
+
+           
+
+          
+        }
+    }
+}
